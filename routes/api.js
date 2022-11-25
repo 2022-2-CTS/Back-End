3a51a8dfcab6ddfb8531ebac56a8211d89ceb423
leejyohan <86225337+leejyohan@users.noreply.github.com>
2022-11-25 19:25:29 +0900

    공공데이터포탈 api get

--- routes/api.js
+++ routes/api.js
@@ -9,13 +9,50 @@ app.use(bodyParser.json())
 const cors = require("cors");
 const { application } = require('express');
 
-const registers = [
-    {}
-]
-
-const options = {useUnifiedTopology: true}; 
+// const options = {useUnifiedTopology: true}; 
 // MongoDB 연결
 const MongoClient = require('mongodb').MongoClient;
+
+const convert = require('xml-js');
+const request = require('request');
+//부산 공공 데이터 포털
+//연극
+var axios = require('axios');
+const { xml } = require('cheerio');
+
+var config = {
+  method: 'get',
+  url: 'http://apis.data.go.kr/6260000/BusanCulturePlayDetailService/getBusanCulturePlayDetail?serviceKey=g4PpueQjaKzayfFQRksvGpJ0jDZ%2BGGmRxzFDMU1o80hY8ObYPgFLuyZDB8iKnGoMh5PSeVLp2tp1lToKexwCjQ%3D%3D',
+  headers: { }
+};
+
+axios(config)
+.then(function (response) {
+  var xmlToJson = convert.xml2json(response.data, {compact: true, spaces: 4});
+  //var tmp = xmlToJson[0]
+  xmlToJson = JSON.parse(xmlToJson)
+  console.log(xmlToJson.response.body.items.item[1])
+  const data = {
+    category:'연극',
+    url:xmlToJson.response.body.items.item.dabom_url._text,
+    data:{
+      title:xmlToJson.response.body.items.item.title._text,
+      op_st_dt:xmlToJson.response.body.items.item.op_st_dt._text,
+      op_ed_dt:xmlToJson.response.body.items.item.op_ed_dt._text,
+      showtime:xmlToJson.response.body.items.item.showtime._text,
+      price:xmlToJson.response.body.items.item.price._text
+    }
+  }
+  
+  console.log(data)
+
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
+
 
 // DB connection URL
 // admin, cts1234
@@ -35,7 +72,6 @@ app.listen(3000, function () {
 MongoClient.connect(URL, function (error, client) {
     // error 출력
     if (error) return console.log(error);
-
     // BusanCultureMap 이라는 데이터베이스(폴더)에 접근할게요!
     db = client.db('BusanCultureMap');
 });
