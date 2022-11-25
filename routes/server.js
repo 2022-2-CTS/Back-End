61b8208b1b0beba45a9c2a5c28df6a5e73020a82
이지호 <jiho9707@gmail.com>
2022-11-25 15:26:00 +0900

    jwt, 자동로그인 기능 구현

--- routes/server.js
+++ routes/server.js
@@ -3,8 +3,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 var bodyParser = require('body-parser')
 var router = express.Router();
+const cookieParser = require("cookie-parser")
+const jwt = require('jsonwebtoken')
 
-
+app.use(cookieParser())
 app.use(bodyParser.json())
 
 const cors = require("cors");
@@ -14,7 +16,7 @@ const registers = [
     {}
 ]
 
-const options = {useUnifiedTopology: true}; 
+const options = { useUnifiedTopology: true };
 // MongoDB 연결
 const MongoClient = require('mongodb').MongoClient;
 
@@ -26,11 +28,11 @@ var URL = 'mongodb+srv://admin:cts1234@cts.1xmwczv.mongodb.net/?retryWrites=true
 // 어떤 데이터베이스에 저장할 것인가?
 var db;
 
-    // 8080 port에 서버를 띄우자!
-    app.listen(3031, function () {
-        // 서버가 열렸을 때 할 일
-        console.log('listening on 3000');
-    });
+// 8080 port에 서버를 띄우자!
+app.listen(3000, function () {
+    // 서버가 열렸을 때 할 일
+    console.log('listening on 3000');
+});
 
 // error : 에러 발생 시, 어떤 에러인지 알려줌
 MongoClient.connect(URL, function (error, client) {
@@ -51,39 +53,90 @@ MongoClient.connect(URL, function (error, client) {
 
 });
 
-app.get("/api/test", function(req, res) {
-    res.send("Hello");
-});
+app.post("/api/register", function (req, res) {
 
-app.get("/api/register", function(req, res){
-    res.send({
-        mid:3,
-        memberName:"이지호"
-    });
-});
-
-app.post("/api/register", function(req, res){
-    
     const Rid = req.body.Rid;
     const Rpw = req.body.Rpw;
     const Rphone = req.body.Rphone;
     console.log(Rid, Rpw, Rphone);
-    db.collection('user').insertOne({id : Rid, pw : Rpw, phone : Rphone}, () => {
+    db.collection('user').insertOne({ id: Rid, pw: Rpw, phone: Rphone }, () => {
         console.log('저장완료')
     })
 })
 
 
-app.post("/api/check", function(req, res){
+app.post("/api/check", function (req, res) {
     const Rid = req.body.Rid;
     console.log(Rid);
-    db.collection('user').find({}, {_id:false, id:true, pw:false, phone:false}).toArray(function(err, result){
-        if(err){
+    db.collection('user').find({}, { _id: false, id: true, pw: false, phone: false }).toArray(function (err, result) {
+        if (err) {
             throw err;
         }
         console.log(result)
         res.send(result);
     })
 });
+
+
+app.get('/api/account', function (req, res) {
+    console.log(req.cookies)
+    if (req.cookies && req.cookies.token) {
+        jwt.verify(req.cookies.token, "abc12345678", (err, decoded) => {
+            if (!err) {
+                res.send(decoded);
+            } else {
+                return res.send(401);
+            }
+        })
+    } else {
+        res.send(401);
+    }
+})
+
+app.post('/api/account', (req, res) => {
+    const loginId = req.body.lId;
+    const loginPw = req.body.lPw;
+
+    console.log(loginId)
+    console.log(loginPw)
+
+    db.collection('user').find({}, { _id: false, id: true, pw: false, phone: false }).toArray(function (err, result) {
+        if (err) {
+            throw err;
+        }
+        //console.log(result)
+        const members = result;
+        const onlyIdMatch = members.find(m => m.id === loginId && m.pw != loginPw);
+        const onlyPwMatch = members.find(m => m.id != loginId && m.pw === loginPw);
+        const member = members.find(m => m.id === loginId && m.pw === loginPw);
+        console.log(member)
+
+        if(onlyIdMatch){
+            res.send("only id match");
+        }
+        else if(onlyPwMatch){
+            res.send("only pw match");
+        }
+        else if (member) {
+            const options = {
+                domain: "localhost",
+                path: "/",
+                httpOnly: true,
+            }
+            const token = jwt.sign({
+                id: member.id,
+            }, "abc12345678", {
+                expiresIn: "30m",
+                issuer: "CTS"
+            })
+            res.cookie("token", token, options);
+            res.send(member.id);
+        } else {
+            res.send(404);
+        }
+    })
+
+})
+
 
 module.exports = router;
