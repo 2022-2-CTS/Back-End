dcbeed9ec52679068065df1a7db75c84508336f4
leejyohan <86225337+leejyohan@users.noreply.github.com>
2022-11-30 12:42:21 +0900

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -24,21 +24,39 @@ const Exhibitdata = [];
 var axios = require('axios');
 const { xml } = require('cheerio');
 const key='g4PpueQjaKzayfFQRksvGpJ0jDZ%2BGGmRxzFDMU1o80hY8ObYPgFLuyZDB8iKnGoMh5PSeVLp2tp1lToKexwCjQ%3D%3D'
-
-//연극 데이터 10개씩
-var config = {
-  method: 'get',
-  url: 'http://apis.data.go.kr/6260000/BusanCulturePlayDetailService/getBusanCulturePlayDetail?serviceKey='+key,
-  headers: {}
-};
-axios(config)
+var list;
+//total받기
+async function gettotal(){
+  console.log("gettotal")
+  var config = {
+    method: 'get',
+    url: 'http://apis.data.go.kr/6260000/BusanCulturePlayDetailService/getBusanCulturePlayDetail?serviceKey='+key,
+    headers: {}
+  };
+  axios(config)
   .then(function (response) {
     var xmlToJson = convert.xml2json(response.data, { compact: true, spaces: 4 });
     //var tmp = xmlToJson[0]
     xmlToJson = JSON.parse(xmlToJson)
+    list = Number(xmlToJson.response.body.totalCount._text);
+    console.log(list,"토탈")
+})
+}
 
-    var list = Number(xmlToJson.response.body.numOfRows._text);
-
+//total 갯수만큼 받아오기
+async function getset(){
+  await console.log("getset")
+  await console.log(list)
+  var config = {
+    method: 'get',
+    url: 'http://apis.data.go.kr/6260000/BusanCulturePlayDetailService/getBusanCulturePlayDetail?serviceKey='+key+'&numOfRows='+list,
+    headers: {}
+  };
+  axios(config)
+  .then( await function (response) {
+    var xmlToJson = convert.xml2json(response.data, { compact: true, spaces: 4 });
+    //var tmp = xmlToJson[0]
+    xmlToJson = JSON.parse(xmlToJson)
     for (var i = 0; i < list; i++) {
       const tmpData = {
         category: '연극',
@@ -53,108 +71,145 @@ axios(config)
       }
       Playdata.push(tmpData);
     }
+    console.log(Playdata,"데이터")
   })
-  .catch(function (error) {
-    console.log(error);
-  });
 
-  // 콘서트
-  var config = {
-    method: 'get',
-    url: 'http://apis.data.go.kr/6260000/BusanCultureConcertDetailService/getBusanCultureConcertDetail?_wadl=json&serviceKey='+key,
-    headers: { }
-  };
-  axios(config)
-  .then(function (response) {
-    var xmlToJson = convert.xml2json(response.data, { compact: true, spaces: 4 });
-    //var tmp = xmlToJson[0]
-    xmlToJson = JSON.parse(xmlToJson)
+}
+async function all(){
+  await gettotal()
+  await getset()
+}
+all()
+//연극 데이터 10개씩
+// var config = {
+//   method: 'get',
+//   url: 'http://apis.data.go.kr/6260000/BusanCulturePlayDetailService/getBusanCulturePlayDetail?serviceKey='+key+'&numOfRows=500',
+//   headers: {}
+// };
+// axios(config)
+//   .then(function (response) {
+//     var xmlToJson = convert.xml2json(response.data, { compact: true, spaces: 4 });
+//     //var tmp = xmlToJson[0]
+//     xmlToJson = JSON.parse(xmlToJson)
 
-    var list = Number(xmlToJson.response.body.numOfRows._text);
+//     var list = Number(xmlToJson.response.body.numOfRows._text);
 
-    for (var i = 0; i < list; i++) {
-      const tmpData = {
-        category: '콘서트',
-        url: xmlToJson.response.body.items.item[i].dabom_url._text,
-        data: {
-          title: xmlToJson.response.body.items.item[i].title._text,
-          op_st_dt: xmlToJson.response.body.items.item[i].op_st_dt._text,
-          op_ed_dt: xmlToJson.response.body.items.item[i].op_ed_dt._text,
-          showtime: xmlToJson.response.body.items.item[i].showtime._text,
-          price: xmlToJson.response.body.items.item[i].price._text
-        }
-      }
-      Concertdata.push(tmpData);
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+//     for (var i = 0; i < list; i++) {
+//       const tmpData = {
+//         category: '연극',
+//         url: xmlToJson.response.body.items.item[i].dabom_url._text,
+//         data: {
+//           title: xmlToJson.response.body.items.item[i].title._text,
+//           op_st_dt: xmlToJson.response.body.items.item[i].op_st_dt._text,
+//           op_ed_dt: xmlToJson.response.body.items.item[i].op_ed_dt._text,
+//           showtime: xmlToJson.response.body.items.item[i].showtime._text,
+//           price: xmlToJson.response.body.items.item[i].price._text
+//         }
+//       }
+//       Playdata.push(tmpData);
+//     }
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+  // // 콘서트
+  // var config = {
+  //   method: 'get',
+  //   url: 'http://apis.data.go.kr/6260000/BusanCultureConcertDetailService/getBusanCultureConcertDetail?_wadl=json&serviceKey='+key,
+  //   headers: { }
+  // };
+  // axios(config)
+  // .then(function (response) {
+  //   var xmlToJson = convert.xml2json(response.data, { compact: true, spaces: 4 });
+  //   //var tmp = xmlToJson[0]
+  //   xmlToJson = JSON.parse(xmlToJson)
+
+  //   var list = Number(xmlToJson.response.body.numOfRows._text);
+
+  //   for (var i = 0; i < list; i++) {
+  //     const tmpData = {
+  //       category: '콘서트',
+  //       url: xmlToJson.response.body.items.item[i].dabom_url._text,
+  //       data: {
+  //         title: xmlToJson.response.body.items.item[i].title._text,
+  //         op_st_dt: xmlToJson.response.body.items.item[i].op_st_dt._text,
+  //         op_ed_dt: xmlToJson.response.body.items.item[i].op_ed_dt._text,
+  //         showtime: xmlToJson.response.body.items.item[i].showtime._text,
+  //         price: xmlToJson.response.body.items.item[i].price._text
+  //       }
+  //     }
+  //     Concertdata.push(tmpData);
+  //   }
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
   
 
-  // 뮤지컬
-  var config = {
-    method: 'get',
-    url: 'http://apis.data.go.kr/6260000/BusanCultureMusicalDetailService/getBusanCultureMusicalDetail?_wadl=json&serviceKey='+key,
-    headers: { }
-  };
-  axios(config)
-  .then(function (response) {
-    var xmlToJson = convert.xml2json(response.data, { compact: true, spaces: 4 });
-    //var tmp = xmlToJson[0]
-    xmlToJson = JSON.parse(xmlToJson)
+  // // 뮤지컬
+  // var config = {
+  //   method: 'get',
+  //   url: 'http://apis.data.go.kr/6260000/BusanCultureMusicalDetailService/getBusanCultureMusicalDetail?_wadl=json&serviceKey='+key,
+  //   headers: { }
+  // };
+  // axios(config)
+  // .then(function (response) {
+  //   var xmlToJson = convert.xml2json(response.data, { compact: true, spaces: 4 });
+  //   //var tmp = xmlToJson[0]
+  //   xmlToJson = JSON.parse(xmlToJson)
 
-    var list = Number(xmlToJson.response.body.numOfRows._text);
+  //   var list = Number(xmlToJson.response.body.numOfRows._text);
 
-    for (var i = 0; i < list; i++) {
-      const tmpData = {
-        category: '뮤지컬',
-        url: xmlToJson.response.body.items.item[i].dabom_url._text,
-        data: {
-          title: xmlToJson.response.body.items.item[i].title._text,
-          op_st_dt: xmlToJson.response.body.items.item[i].op_st_dt._text,
-          op_ed_dt: xmlToJson.response.body.items.item[i].op_ed_dt._text,
-          showtime: xmlToJson.response.body.items.item[i].showtime._text,
-          price: xmlToJson.response.body.items.item[i].price._text
-        }
-      }
-      Musicaldata.push(tmpData);
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  //   for (var i = 0; i < list; i++) {
+  //     const tmpData = {
+  //       category: '뮤지컬',
+  //       url: xmlToJson.response.body.items.item[i].dabom_url._text,
+  //       data: {
+  //         title: xmlToJson.response.body.items.item[i].title._text,
+  //         op_st_dt: xmlToJson.response.body.items.item[i].op_st_dt._text,
+  //         op_ed_dt: xmlToJson.response.body.items.item[i].op_ed_dt._text,
+  //         showtime: xmlToJson.response.body.items.item[i].showtime._text,
+  //         price: xmlToJson.response.body.items.item[i].price._text
+  //       }
+  //     }
+  //     Musicaldata.push(tmpData);
+  //   }
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
   
 
-  // 전시
-  var config = {
-    method: 'get',
-    url: 'http://apis.data.go.kr/6260000/BusanCultureExhibitDetailService/getBusanCultureExhibitDetail?_wadl=json&serviceKey='+key,
-    headers: { }
-  };
-  axios(config)
-  .then(function (response) {
-    var xmlToJson = convert.xml2json(response.data, { compact: true, spaces: 4 });
-    //var tmp = xmlToJson[0]
-    xmlToJson = JSON.parse(xmlToJson)
-    var list = Number(xmlToJson.response.body.numOfRows._text);
-    for (var i = 0; i < list; i++) {
-      const tmpData = {
-        category: '전시',
-        url: xmlToJson.response.body.items.item[i].dabom_url._text,
-        data: {
-          title: xmlToJson.response.body.items.item[i].title._text,
-          op_st_dt: xmlToJson.response.body.items.item[i].op_st_dt._text,
-          op_ed_dt: xmlToJson.response.body.items.item[i].op_ed_dt._text,
-          price: xmlToJson.response.body.items.item[i].price._text
-        }
-      }
-      Exhibitdata.push(tmpData);
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  // // 전시
+  // var config = {
+  //   method: 'get',
+  //   url: 'http://apis.data.go.kr/6260000/BusanCultureExhibitDetailService/getBusanCultureExhibitDetail?_wadl=json&serviceKey='+key,
+  //   headers: { }
+  // };
+  // axios(config)
+  // .then(function (response) {
+  //   var xmlToJson = convert.xml2json(response.data, { compact: true, spaces: 4 });
+  //   //var tmp = xmlToJson[0]
+  //   xmlToJson = JSON.parse(xmlToJson)
+  //   var list = Number(xmlToJson.response.body.numOfRows._text);
+  //   for (var i = 0; i < list; i++) {
+  //     const tmpData = {
+  //       category: '전시',
+  //       url: xmlToJson.response.body.items.item[i].dabom_url._text,
+  //       data: {
+  //         title: xmlToJson.response.body.items.item[i].title._text,
+  //         op_st_dt: xmlToJson.response.body.items.item[i].op_st_dt._text,
+  //         op_ed_dt: xmlToJson.response.body.items.item[i].op_ed_dt._text,
+  //         price: xmlToJson.response.body.items.item[i].price._text
+  //       }
+  //     }
+  //     Exhibitdata.push(tmpData);
+  //   }
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
 
 // DB connection URL
 // admin, cts1234
@@ -168,15 +223,15 @@ router.get('/play',function(get,res){
   res.send(Playdata);
   // console.log(Playdata)
 });
-router.get('/concert',function(get,res){
-  res.send(Concertdata);
-});
-router.get('/musical',function(get,res){
-  res.send(Musicaldata);
-});
-router.get('/exhibit',function(get,res){
-  res.send(Exhibitdata);
-});
+// router.get('/concert',function(get,res){
+//   res.send(Concertdata);
+// });
+// router.get('/musical',function(get,res){
+//   res.send(Musicaldata);
+// });
+// router.get('/exhibit',function(get,res){
+//   res.send(Exhibitdata);
+// });
 
 // error : 에러 발생 시, 어떤 에러인지 알려줌
 MongoClient.connect(URL, function (error, client) {
